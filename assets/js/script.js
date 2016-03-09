7606f974aa25291d3c5972056cbab6cca2f67524
Brandon Ly <brandon@Brandons-MacBook-Pro.local>
2016-03-08 19:57:15 -0800

    restyle mobile menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,11 @@ $('.hamburger-icon').click(function() {
     if ($i % 2 == 0) {
         $(this).addClass('is_active');
         $('.nav-menu').addClass('open_nav');
+        $('.page-wrapper').addClass('menu_open');
     } else {
         $(this).removeClass('is_active');
         $('.nav-menu').removeClass('open_nav');
+        $('.page-wrapper').removeClass('menu_open');
     }
     $i++;
 });
@@ -16,4 +18,5 @@ $('.nav-menu a').click(function(){
     $i++;
     $('.hamburger-icon').removeClass('is_active');
     $('.nav-menu').removeClass('open_nav');
+    $('.page-wrapper').removeClass('menu_open');
 });
