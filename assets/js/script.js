ca276896199dd27ea766fef78bfbadbc6311e11a
Brandon Ly <brandon@Brandons-MacBook-Pro.local>
2016-05-25 11:52:24 -0700

    add smooth scrolling

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,18 @@ $('.nav-menu a').click(function(){
     $('.nav-menu').removeClass('open_nav');
     $('.page-wrapper').removeClass('menu_open');
 });
+
+$('a[href*=\\#]:not([href=\\#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 800);
+            return false;
+        }
+    }
+});
